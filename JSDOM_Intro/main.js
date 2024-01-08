0b1aed01e42858733f1a93b3630ecdbb4c0f7107
JKuriata <judytaizabellakuriata@outlook.com>
2024-01-08 11:37:24 +0000

    Practicing with Javascript

---
+++ JSDOM_Intro/main.js
@@ -0,0 +1,32 @@
+// console.log(document)
+
+// console.log(document.getElementById("listWrapper"))
+
+// console.log(document.getElementsByTagName("p"[1]))
+
+// const header = document.getElementById("heading");
+
+// header.textContent = "New heading";
+// header.style.border = "3px solid green";
+
+// //text content would not do the same formatting as HTML like innerHTML which adds the list items, text content would actually print <li></li> with the script. So it should only really be used for changing text not formatting.
+
+// const list = document.getElementById("listWrapper");
+
+// list.innerHTML = "<li>New list item</li> <li>Second list element</li>";
+// list.style.backgroundColor = "yellow";
+
+// const listItems = document.getElementsByClassName("listItem");
+// listItems[1].style.display= "none";
+
+// Adding event listener -
+// document.addEventListener(event,function)
+
+// document.addEventListener("click", function() {console.log("You have clicked")})
+
+// ---------------------
+// const heading= document.getElementById("heading");
+// const input = document.getElementById("inputBox");
+// const submitBtn = document.getElementById("submit")
+
+// submitBtn.addEventListener("click", () => {heading.textContent = input.value})
